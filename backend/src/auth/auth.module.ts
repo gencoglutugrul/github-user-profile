48183938184bb13966b43a4db9eee6e10a78894a
Tuğrul Gençoğlu <gencoglutugrul@gmail.com>
2021-06-13 17:29:04 +0300

    jwt token expire increased

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -16,7 +16,7 @@ import { JwtStrategy } from './strategies/jwt.strategy';
     PassportModule,
     JwtModule.register({
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '24h' },
     }),
   ],
   providers: [AuthService, LocalStrategy, JwtStrategy],
