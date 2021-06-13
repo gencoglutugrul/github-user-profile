a3516f81eb837400f429701ec406f0a30d6f5a89
Tuğrul Gençoğlu <gencoglutugrul@gmail.com>
2021-06-13 19:33:31 +0300

    id is necessary for component key

--- backend/src/github/dto/github-user-repo.dto.ts
+++ backend/src/github/dto/github-user-repo.dto.ts
@@ -3,6 +3,9 @@ import { Exclude, Expose } from "class-transformer";
 @Exclude()
 export class GithubUserRepoDto {
     @Expose()
+    readonly id: number;
+
+    @Expose()
     readonly name: string;
 
     @Expose()
